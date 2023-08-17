a1294a4481688d7b1dbd041c003922c5ff3884f0
Gahade <angrygahade@gmail.com>
2023-08-17 08:12:25 +0200

    Add contextmanager timer

--- main.py
+++ main.py
@@ -4,26 +4,22 @@
 Використовуйте контекстний менеджер для вимірювання часу виконання певного фрагменту коду.
 Реалізувати контекстний менеджер потрібно 2 способами, за допомогою декоратора contextmanager та за допомогою класу.
 """
-from time import time, sleep
+from contextlib import contextmanager
+import time
 
 
-class Timer:
-    def __init__(self):
-        self.interval = 0
-
-    def __enter__(self):
-        self.start = time()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        end = time()
-        self.interval = end - self.start
-        print(f"Operation time = {self.interval:.2f} seconds")
+@contextmanager
+def timer():
+    start = time.time()
+    print("Your operation has started!")
+    yield
+    end = time.time()
+    print(f"Operation time = {end - start:.2f} seconds")
 
 
 my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
 
-with Timer() as timer:
-    sleep(2)
+with timer() as t:
+    time.sleep(1)
     sorted_list = sorted(my_list)
 
