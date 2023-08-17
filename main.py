bd48bfbff7204762ea881a34865c934ff0ed00dd
Gahade <angrygahade@gmail.com>
2023-08-17 08:00:34 +0200

    Add contextmanager Timer

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+"""
+Реалізувати менеджер контексту Timer, який вимірює час виконання блоку коду.
+Контекстний менеджер повинен виводити час, що минув, при виході з контексту.
+Використовуйте контекстний менеджер для вимірювання часу виконання певного фрагменту коду.
+Реалізувати контекстний менеджер потрібно 2 способами, за допомогою декоратора contextmanager та за допомогою класу.
+"""
+from time import time, sleep
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Timer:
+    def __init__(self):
+        self.interval = 0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __enter__(self):
+        self.start = time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        end = time()
+        self.interval = end - self.start
+        print(f"Operation time = {self.interval:.2f} seconds")
+
+
+my_list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+with Timer() as timer:
+    sleep(2)
+    sorted_list = sorted(my_list)
+
